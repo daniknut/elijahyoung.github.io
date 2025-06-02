4477985499c28bbd333bf2e35e1119b5a41d387e
daniknut <daniknut@mit.edu>
2025-06-01 20:23:19 -0700

    p

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const stickyImage = document.getElementById('stickyImage');
-const stopSection = document.getElementById('stopSection');
+// const stickyImage = document.getElementById('stickyImage');
+// const stopSection = document.getElementById('stopSection');
 
-window.addEventListener('scroll', () => {
-  const stopY = stopSection.offsetTop;
+// window.addEventListener('scroll', () => {
+//   const stopY = stopSection.offsetTop;
 
-  if (window.scrollY >= stopY) {
-    // Image has reached the stop point
-    stickyImage.style.position = 'absolute';
-    stickyImage.style.top = stopY + 'px';
-  } else {
-    // Image should stick to the top
-    stickyImage.style.position = 'fixed';
-    stickyImage.style.top = '100';
-  }
-});
+//   if (window.scrollY >= stopY) {
+//     // Image has reached the stop point
+//     stickyImage.style.position = 'absolute';
+//     stickyImage.style.top = stopY + 'px';
+//   } else {
+//     // Image should stick to the top
+//     stickyImage.style.position = 'fixed';
+//     stickyImage.style.top = '100';
+//   }
+// });
